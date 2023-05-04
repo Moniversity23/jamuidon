0770b5cbc8b2dc4da8fc8358cd27c83986fa5977
Moniversity23 <124564479+Moniversity23@users.noreply.github.com>
2023-05-04 12:59:05 +0000

    Add files via upload

---
+++ jamb.js
@@ -0,0 +1,15 @@
+let searchBox = document.querySelector('.search');
+let submitBtn = document.querySelector('.submit');
+
+
+searchBox.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let regNumber = searchBox.querySelector('input[type=text]').Value;
+        if (regNumber /= ahvfgfg ) {
+            searchBox.classList.remove('hide')
+        }else {
+            return true
+        }
+
+    
+})
